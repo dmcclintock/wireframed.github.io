b368050c30d033007648df9e8f67b5dafceefb82
Garrett Boatman <garrett.boatman@mac.com>
2013-06-27 11:40:07 -0700

    Another Update
    Added Royal Slider
    Small Changes

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,19 +1,18 @@
-
-// Takes the data attribute and turnes it into a hidden description overlay.
-$('[data-description]').each(function(){
-	$(this).html(
-		$(this).html() + '<div class="wf-description">' + $(this).data('description') + '</div>'
+$(document).ready(function(){
+  // Takes the data attribute and turnes it into a hidden description overlay.
+	$('[data-description]').each(function(){
+		$(this).html(
+			$(this).html() + '<div class="wf-description">' + $(this).data('description') + '</div>'
+		);
+	});
+	
+	// Fades in the description on hover
+	$('.wf-description').hover(
+	  function () {
+	    $(this).addClass('view-description');
+	  },
+	  function () {
+	  	$(this).removeClass('view-description');
+	  }
 	);
-});
-
-// Fades in the description on hover
-$('.wf-description').hover(
-  function () {
-    $(this).addClass('view-description');
-  },
-  function () {
-  	$(this).removeClass('view-description');
-  }
-);
-
-
+})
